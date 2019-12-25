ef50e001d1f75421f2917063f95e679521f8f2e5
piotrek19 <dziobap@gmail.com>
2019-12-25 14:08:19 +0100

    initial commit

---
+++ src/main/java/net/dzioba/petclinicmicro/petclinicmicronotificationgatewayapp/PetClinicMicroNotificationGatewayAppApplication.java
@@ -0,0 +1,13 @@
+package net.dzioba.petclinicmicro.petclinicmicronotificationgatewayapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicMicroNotificationGatewayAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicMicroNotificationGatewayAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/dzioba/petclinicmicro/petclinicmicronotificationgatewayapp/PetClinicMicroNotificationGatewayAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.dzioba.petclinicmicro.petclinicmicronotificationgatewayapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicMicroNotificationGatewayAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
